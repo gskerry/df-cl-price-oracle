9512355527a280d226931e92a4b37b3071ccef32
greg <gregory.skerry@gmail.com>
2020-03-18 20:47:02 -0700

    removing price-oracle files

--- oracle/migrations/2_invoice_price_migration.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-const InvoicePrice = artifacts.require('InvoicePrice')
-
-module.exports = (deployer, network) => {
-  deployer.deploy(InvoicePrice, process.env.ORACLE, process.env.JOB_ID)
-}
